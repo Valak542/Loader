999dc5ea2a88d318ebac8d75eaaf146db33af7c7
Valak542 <lilmissvt@gmail.com>
2025-09-19 17:57:31 +1000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,21 @@
 local RivalsPlaceIds = {17625359962}
 local function RivalsLoad()
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86295d9fdddf4ebfea06d83a70f79332.lua"))()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86295d9fdddf4ebfea06d83a70f79332.lua"))()
+    end)
+    if not success then
+        warn("Failed to load the Rivals script. Error:", err)
+    end
 end
 
 local FF2PlaceIds = {8204899140, 8206123457}
 local function FF2Load()
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/211b4b1877e20d6b04152fee50c56302.lua"))()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/211b4b1877e20d6b04152fee50c56302.lua"))()
+    end)
+    if not success then
+        warn("Failed to load the FF2 script. Error:", err)
+    end
 end
 
 if table.find(RivalsPlaceIds, game.PlaceId) then
